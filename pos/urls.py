4683952fec6a0f91f22d91d54df3b7b5aebeec08
Mohamed shakir <shakirsar225974@gmail.com>
2026-04-01 16:22:01 +0530

    Innial commit

---
+++ pos/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('',views.product_list,name='product_list'),
+    path('add/<int:product_id>/' , views.add_to_cart , name='add_to_cart'),
+    path('cart/' , views.cart_view , name='cart_view'),
+    path('remove/<int:product_id>/' , views.remove_from_cart , name="remove_from_cart"),
+    path('clear/' , views.clear_cart , name='clear_cart')
+
+]
---
+++ pos/views.py
@@ -0,0 +1,74 @@
+from django.shortcuts import render,redirect
+from .models import Product
+
+# function to display product list
+def product_list(request):
+    products = Product.objects.all()
+    return render(request,'products.html',{'products':products})
+
+
+# function to get cart items
+def get_cart(request):
+    return request.session.setdefault('cart',{})
+
+# function to add to cart
+def add_to_cart(request , product_id):
+    cart = get_cart(request)
+
+    product_id = str(product_id)
+
+    if product_id in cart:
+        cart[product_id] += 1
+    else:
+        cart[product_id] = 1
+    
+    request.session.modified = True
+
+    return redirect('cart_view')
+
+
+# function to display the cart items
+def cart_view(request):
+    
+    cart = get_cart(request)
+    items = []
+    total = 0
+
+    for product_id , quantity in cart.items():
+        product = Product.objects.get(id = product_id)
+        subTotal = product.price * quantity
+        total += subTotal
+
+        items.append(
+            {
+                'product':product,
+                'quantity': quantity,
+                'subTotal':subTotal
+            }
+        )
+
+    return render(request, 'cart.html', {'items':items , 'total':total})
+
+# function to remove item from cart
+def remove_from_cart(request,product_id):
+
+    cart = get_cart(request)
+    product_id = str(product_id)
+
+    if product_id in cart:
+        del cart[product_id] # remiving item
+
+        request.session.modified = True
+    
+    return redirect('cart_view')
+
+def clear_cart(request):
+
+    cart = get_cart(request)
+
+    cart.clear() # make empty the cart dictionary
+
+    request.session.modified = True
+    
+    return redirect('cart_view')
+
